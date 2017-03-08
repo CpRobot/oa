1b39e53e48487a966adb073a489d103cd99a4488
Pengchu <3207603685@qq.com>
2017-03-08 15:52:50 +0800

    后台首页添加

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -9,6 +9,9 @@
 
 namespace app\admin\controller;
 
+use think\Db;
+use think\Session;
+
 class Index extends Common {
 
 	/**
@@ -20,7 +23,36 @@ class Index extends Common {
         return $this->fetch();
     }
 
+    /**
+     * 签到操作
+     */
+    public function signIn() {
+    	$uid = Session::get('userinfo.id');
+
+    	// 判断今天是否已签到
+    	if (!$this->checkSignIn($uid)) {
+    		exit(json_encode(array('status' => 0, 'msg' => '今天已经签到过了！')));
+    	} else {
+    		$sucess_msg = '签到成功';
+    		$data = [
+    			'uid' => $uid,
+    			'action' => 'sign_in',
+    			'note' => $sucess_msg,
+    			'login_ip' => get_client_ip(),
+    			'addtime' => date('Y-m-d H:i:s', time())
+    		];
+    		$flag = Db::name('log') -> insert($data);
+    		if ($flag) {
+    			exit(json_encode(array('status' => 1, 'msg' => $sucess_msg)));
+    		} else {
+    			exit(json_encode(array('status' => 0, 'msg' => "签到失败，请稍候再试！")));
+    		}
+    	}
+    }
 
 
 
+    protected function checkSignIn($uid) {
+    	return Db::name('log') -> where('id', $uid) -> whereTime('addtime', 'today') -> count();
+    }
 }
